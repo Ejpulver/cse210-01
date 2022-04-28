23d677f336ac00a7f98c902358734bffc66c69be
Ejpulver <pul15004@byui.edu>
2022-04-28 10:34:13 -0600

    created board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,17 @@
 """ introduction: Ponder and Prove Easton Pulver"""
 
 def board():
-    board = []
+    board_first_row = [1|2|3]
+    board_second_row = [4|5|6]
+    board_third_row = [7|8|9]
+    print(board_first_row)
+    print('-+-+-')
+    print(board_second_row)
+    print('-+-+-')
+    print(board_third_row)
+
+board()
+
 
 
 
